22f56cf0cab815cc3befca940b894dccd9be53da
YanDevv <ybritogomes@gmail.com>
2024-09-15 02:24:38 -0300

    feat(toastify): Added toastify layer

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App.tsx'
+
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<App />
+		<ToastContainer />
 	</StrictMode>,
 )
